423649a965b703564e55cac94ab2d19402496517
Zygimantas Daunoras <daunoraszygimantas@gmail.com>
2020-10-30 14:23:13 +0000

    made quadrants that turn black when hovering over it

--- index.java
+++ index.java
@@ -5,14 +5,34 @@ void setup() {
 void draw() {
 	background(255);
 	stroke(0);
+	fill(0);
 
-  //start creating the lines
+//start creating the lines
 
+	// horizontal lines
+	line(0, 0, 399, 0);
+	line(0, 399, 399, 399);
 
-  //Fill a black color
-  
+	// vertical lines
+	line(0, 0, 0, 399);
+	line(399, 0, 399, 399);
 
-  //Build your conditional statement 
+	//intersection
+	line(200, 0, 200, 399);
+	line(0, 200, 399, 200);
+
+	println(mouseX, mouseY);
+
+//Build your conditional statement 
 	
+	if(mouseX < 200 && mouseY <= 200) {
+		rect(0, 0, 200, 200);
+	}else if(mouseX >= 200 && mouseY <= 200) {
+		rect(200, 0, 200, 200);
+	}else if(mouseX < 200 && mouseY > 200) {
+		rect(0, 200, 200, 200);
+	}else {
+		rect(200, 200, 200, 200);
+	}
 
 }
